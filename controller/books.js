69d92386ed73ea02f87d902c42356f2c494ed0af
Arjun Raj <arjunrajvilakkat@outlook.com>
2025-09-25 10:34:11 +0530

    display quote from an api in index page

--- controller/books.js
+++ controller/books.js
@@ -1,6 +1,28 @@
+const axios = require("axios");
+
 exports.getHome = (req, res, next) => {
   res.render("index", {
     pageTitle: "Home",
     path: "/",
   });
+};
+
+exports.getQuoteApi = (req, res, next) => {
+  const RECITE_API_URL = process.env.RECITE_API_URL + "/random";
+  axios
+    .get("https://recite.onrender.com/api/v1/random")
+    .then((result) => {
+      const quoteData = result.data;
+
+      res.json(quoteData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error", {
+        pageTitle: "Error !",
+        path: "/",
+        errorCode: 500,
+        errorMessage: "Something went wrong",
+      });
+    });
 };
